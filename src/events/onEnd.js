d456fad06eba19de66d71e30a908922001527cc4
diddntjd99 <diddntjd99@gmail.com>
2024-07-04 11:41:13 +0900

    게임 종료 시 유저를 못찾았을 때 예외처리

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -6,6 +6,10 @@ export const onEnd = (socket) => async () => {
   console.log('클라이언트 연결이 종료되었습니다.');
 
   const user = getUserBySocket(socket);
+  if (!user) {
+    console.log('유저를 찾을 수 없습니다.');
+    return;
+  }
 
   const { x, y } = user.getPosition();
   await updateUserLocation(user.id, x, y);
